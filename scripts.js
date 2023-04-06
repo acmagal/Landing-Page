cc4abfef5e5cfbc3809d89009759837baa0a3257
André Cavalcante <andedis053@gmail.com>
2023-04-06 11:45:50 -0300

    Correções na Responsividade, Inicio do JS.

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function checar (Nome,Email){
+
+    Email = document.querySelector('#email').value
+    Nome = document.querySelector('#nome').value
+
+    if (Nome==123){
+        console.log("certo")
+        
+    }
+    else {
+        console.log("errado")
+        
+    }
+
+    return
+    
+}
+
