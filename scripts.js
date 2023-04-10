1593686bb9808a2d8cc7ea39c6e3c5fea42c5b2a
André Cavalcante <andedis053@gmail.com>
2023-04-10 13:40:21 -0300

    Página de Login/Scripts de login/dataCheck.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,29 @@
-function checar (Nome,Email){
-
-    Email = document.querySelector('#email').value
-    Nome = document.querySelector('#nome').value
-
-    if (Nome==123){
-        console.log("certo")
-        
-    }
-    else {
-        console.log("errado")
-        
-    }
-
-    return
+function logar(){
+    login = document.querySelector('#login').value;
+    senha = document.querySelector('#senha').value;
     
+
+    if(login == "admin" && senha == "admin"){
+        alert('Sucesso');
+    }
 }
+
+function dataCheck(){
+    nome = document.querySelector('#nome').value;
+    email = document.querySelector('#email').value;
+
+    if(nome.length !== 0 && email.length !== 0 && email.includes("@")){
+        alert('Obrigado! Fique de olho em seu E-mail!')
+    }
+
+    else if(email.includes("@") && nome.length == 0){
+        alert('Campo "Nome" está vazio!')
+    }
+
+    else if( nome.length !== 0 && email.length == 0){
+        alert('Campo "E-mail" está vazio!')
+    }
+}
+
+
 
